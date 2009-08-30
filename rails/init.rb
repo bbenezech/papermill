104070ab31df4a4392db5ba3d1b0818c91589473
BBenezech <benoit.benezech@gmail.com>
2009-08-30 15:45:37 +0200

    Locales directory change

--- rails/init.rb
+++ rails/init.rb
@@ -3,7 +3,7 @@ Object.send :include, ObjectExtensions
 Hash.send :include, HashExtensions
 String.send :include, StringExtensions
 
-I18n.load_path = [File.join(File.dirname(__FILE__), '../config/locale/papermill.yml')] + I18n.load_path
+I18n.load_path = [File.join(File.dirname(__FILE__), "../config/locales/papermill.yml")] + I18n.load_path
 
 require 'papermill'
 ActionView::Base.send :include, PapermillHelper
