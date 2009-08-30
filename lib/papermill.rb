d5edd7088034d9345a793fdd004dbd6ece1cf349
BBenezech <benoit.benezech@gmail.com>
2009-08-30 16:02:05 +0200

    debug i18n/gems/engine..

--- lib/papermill.rb
+++
@@ -1,4 +0,0 @@
-require 'papermill/papermill_module'
-require 'papermill/papermill_asset'
-require 'papermill/form_builder'
-require 'papermill/papermill_helper'
--- rails/init.rb
+++ rails/init.rb
@@ -5,6 +5,9 @@ String.send :include, StringExtensions
 
 I18n.load_path = [File.join(File.dirname(__FILE__), "../config/locales/papermill.yml")] + I18n.load_path
 
-require 'papermill'
+require 'papermill/papermill_module'
+require 'papermill/papermill_asset'
+require 'papermill/form_builder'
+require 'papermill/papermill_helper'
 ActionView::Base.send :include, PapermillHelper
 ActiveRecord::Base.send :include, Papermill
