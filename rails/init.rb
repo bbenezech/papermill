79600cd6754609ad6c90b0e73d7d00ac039eb373
BBenezech <benoit.benezech@gmail.com>
2009-08-30 14:48:20 +0200

    Gem

---
+++ rails/init.rb
@@ -0,0 +1,10 @@
+require 'core_extensions'
+Object.send :include, ObjectExtensions
+Hash.send :include, HashExtensions
+String.send :include, StringExtensions
+
+I18n.load_path = [File.join(File.dirname(__FILE__), '../config/locale/papermill.yml')] + I18n.load_path
+
+require 'papermill'
+ActionView::Base.send :include, PapermillHelper
+ActiveRecord::Base.send :include, Papermill
