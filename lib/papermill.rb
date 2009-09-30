14a884a70565c6a1cd2d92e242342d32e78f862c
BENEZECH <bbenezech@fi005780.io.le-figaro.com>
2009-09-30 14:02:06 +0200

    traductions pb

--- lib/papermill.rb
+++ lib/papermill.rb
@@ -1,3 +1,4 @@
+I18n.load_path = [File.join(File.dirname(__FILE__), "../config/locales/papermill.yml")] + I18n.load_path
 require 'core_extensions'
 Object.send :include, ObjectExtensions
 Hash.send :include, HashExtensions
