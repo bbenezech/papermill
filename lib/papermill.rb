dfa2e2483f85b04099918837dff9f26287d166af
Benoit Bénézech <ben@ben.(none)>
2009-11-01 19:15:17 +0100

    reoved formtastic debug

--- lib/papermill.rb
+++ lib/papermill.rb
@@ -5,13 +5,7 @@ Hash.send :include, PapermillHashExtensions
 File.send :include, PapermillFileExtensions
 String.send :include, PapermillStringExtensions
 String.send :include, PapermillStringToUrlNotFound unless String.instance_methods.include? "to_url"
-begin
-  Formtastic::SemanticFormBuilder.send :include, PapermillFormtasticExtensions
-  puts "FORMTASTIC"
-rescue LoadError
- puts "NO FORMTASTIC"
-end
-puts "HEYYY"
+Formtastic::SemanticFormBuilder.send :include, PapermillFormtasticExtensions rescue LoadError, NameError
 require 'papermill/papermill_module'
 require 'papermill/papermill_asset'
 require 'papermill/form_builder'
