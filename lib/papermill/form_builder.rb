5fddada6b63ea9195fb0793afe93149d90956ee1
BENEZECH <bbenezech@FI005780.local>
2009-11-30 17:04:45 +0100

    form builder refactoring

--- lib/papermill/form_builder.rb
+++ lib/papermill/form_builder.rb
@@ -34,116 +34,94 @@ module ActionView::Helpers::FormTagHelper
   end
   
   private
+  
   def papermill_upload_tag(key, options)
-    if key.nil? && (options[:assetable].is_a?(String) || options[:assetable].is_a?(Symbol))
+    
+    if key.nil? && [String, Symbol].include?(options[:assetable].class)
       key = options[:assetable].to_s
       options[:assetable] = nil
     end
- 
-    assetable = options[:assetable] || @template.instance_variable_get("@#{@object_name}")
-    options = PapermillAsset.assetable_papermill_options(assetable && assetable.class.name, key).deep_merge(options)
     
+    assetable = options[:assetable] || @template.instance_variable_get("@#{@object_name}")
     assetable_id = assetable && (assetable.id || assetable.timestamp) || nil
     assetable_type = assetable && assetable.class.base_class.name || nil
-    id = "papermill_#{assetable_type}_#{assetable_id}_#{key}"
-    if options[:thumbnail]
-      w = options[:thumbnail][:width]  || options[:thumbnail][:height] && options[:thumbnail][:aspect_ratio] && (options[:thumbnail][:height] * options[:thumbnail][:aspect_ratio]).to_i || nil
-      h = options[:thumbnail][:height] || options[:thumbnail][:width]  && options[:thumbnail][:aspect_ratio] && (options[:thumbnail][:width]  / options[:thumbnail][:aspect_ratio]).to_i || nil
-      options[:thumbnail][:style] ||= (w || h) && "#{w}x#{h}>" || "original"
-      if options[:inline_css]
-        size = []
-        size << "width:#{w}px" if w
-        size << "height:#{h}px" if h
-        size = size.join("; ")
-        @template.content_for :papermill_inline_css do
-          inline_css = ["\n"]
-          if options[:gallery]
-            vp = options[:gallery][:vpadding].to_i
-            hp = options[:gallery][:hpadding].to_i
-            vm = options[:gallery][:vmargin].to_i
-            hm = options[:gallery][:hmargin].to_i
-            b  = options[:gallery][:border_thickness].to_i
-            gallery_width = (options[:gallery][:width] || w) && "width:#{options[:gallery][:width] || options[:gallery][:columns]*(w+(hp+hm+b)*2)}px;" || ""
-            gallery_height = (options[:gallery][:height] || h) && "min-height:#{options[:gallery][:height] || options[:gallery][:lines]*(h+(vp+vm+b)*2)}px;" || ""
-            inline_css << %{##{id} { #{gallery_width} #{gallery_height} }}
-            inline_css << %{##{id} .asset { margin:#{vm}px #{hm}px; border-width:#{b}px; padding:#{vp}px #{hp}px; #{size}; }}
-          else
-            inline_css << %{##{id}, ##{id} .asset { #{size} }}
-          end
-          inline_css << %{##{id} .name { width:#{w || "100"}px; }}
-          inline_css.join("\n")
-        end
-      end
+    options = PapermillAsset.assetable_papermill_options(assetable && assetable.class.name, key).deep_merge(options)
+    dom_id = "papermill_#{assetable_type}_#{assetable_id}_#{key}"
+    
+    if ot = options[:thumbnail]
+      w = ot[:width]  || ot[:height] && ot[:aspect_ratio] && (ot[:height] * ot[:aspect_ratio]).to_i || nil
+      h = ot[:height] || ot[:width]  && ot[:aspect_ratio] && (ot[:width]  / ot[:aspect_ratio]).to_i || nil
+      ot[:style] ||= (w || h) && "#{w}x#{h}>" || "original"
+      set_papermill_inline_css(dom_id, w, h, options)
+    end
+
+    set_papermill_inline_js(dom_id, compute_papermill_create_url(assetable_id, assetable_type, key, options), options)
+
+    html = {}
+    html[:upload_button] = %{<div id="#{dom_id}-button-wrapper" class="papermill-button-wrapper" style="height: #{options[:swfupload][:button_height]}px;"><span id="browse_for_#{dom_id}" class="swf_button"></span></div>}
+    html[:container] = @template.content_tag(:div, :id => dom_id, :class => "papermill-#{key.to_s} #{(options[:thumbnail] ? "papermill-thumb-container" : "papermill-asset-container")} #{(options[:gallery] ? "papermill-multiple-items" : "papermill-unique-item")}") do
+      conditions = {:assetable_type => assetable_type, :assetable_id => assetable_id}
+      conditions.merge!({:assetable_key => key.to_s}) if key
+      @template.render :partial => "papermill/asset", :collection => PapermillAsset.all(:conditions => conditions), :locals => { :thumbnail_style => options[:thumbnail] && options[:thumbnail][:style] }
     end
     
-    url_options = {
-      :escape => false,
-      :controller => "/papermill", 
-      :action => "create", 
-      :asset_class => (options[:class_name] || PapermillAsset).to_s,
-      :gallery => !!options[:gallery], 
-      :thumbnail_style => options[:thumbnail] && options[:thumbnail][:style]
-    }
+    if options[:gallery]
+      html[:dashboard] = {}
+      html[:dashboard][:mass_edit] = %{<a onclick="modify_all('#{dom_id}'); return false;" style="cursor:pointer">#{I18n.t("papermill.modify-all")}</a><select id="batch_#{dom_id}">#{options[:mass_editable_fields].map do |field|
+                %{<option value="#{field.to_s}">#{I18n.t("papermill.#{field.to_s}", :default => field.to_s)}</option>}
+              end.join("\n")}</select>}
+      html[:dashboard][:mass_delete] = %{<a onclick="mass_delete('#{dom_id}', '#{@template.escape_javascript I18n.t("papermill.delete-all-confirmation")}'); return false;" style="cursor:pointer">#{I18n.t("papermill.delete-all")}</a>}
+      html[:dashboard] = @template.content_tag(:ul, options[:dashboard].map{|action| @template.content_tag(:li, html[:dashboard][action], :class => action.to_s) }.join("\n"), :class => "dashboard")
+    end
     
-    url_options.merge!({
-      :assetable_id => assetable_id, 
-      :assetable_type => assetable_type
-    }) if assetable
-    
-    url_options.merge!({
-      :assetable_key => key
-    }) if key
-    
-    
-    html = {}
-    create_url = @template.url_for(url_options)
     if assetable && assetable.new_record? && !@timestamped
       @timestamp_field = @template.hidden_field(assetable_type.underscore, :timestamp, :value => assetable.timestamp)
       @timestamped = true
     end
     
-    conditions = {:assetable_type => assetable_type, :assetable_id => assetable_id}
-    conditions.merge!({:assetable_key => key.to_s}) if key
-    collection = PapermillAsset.all(:conditions => conditions)
-    
-    html[:upload_button] = %{<div id="#{id}-button-wrapper" class="papermill-button-wrapper" style="height: #{options[:swfupload][:button_height]}px;"><span id="browse_for_#{id}" class="swf_button"></span></div>}
-    html[:container] = @template.content_tag(:div, :id => id, :class => "papermill-#{key.to_s} #{(options[:thumbnail] ? "papermill-thumb-container" : "papermill-asset-container")} #{(options[:gallery] ? "papermill-multiple-items" : "papermill-unique-item")}") {
-      @template.render :partial => "papermill/asset", :collection => collection, :locals => { :thumbnail_style => options[:thumbnail] && options[:thumbnail][:style] }
+	  %{<div class="papermill">#{@timestamp_field.to_s + options[:form_helper_elements].map{|element| html[element] || ""}.join("\n")}</div>}
+  end
+  
+  
+  def compute_papermill_create_url(assetable_id, assetable_type, key, options)
+    create_url_options = { 
+      :escape => false, :controller => "/papermill", :action => "create", 
+      :asset_class => (options[:class_name] || PapermillAsset).to_s,
+      :gallery => !!options[:gallery], :thumbnail_style => options[:thumbnail] && options[:thumbnail][:style]
     }
-    
-    if options[:gallery]
-      html[:dashboard] = {}
-      html[:dashboard][:mass_edit] = %{<a onclick="modify_all('#{id}'); return false;" style="cursor:pointer">#{I18n.t("papermill.modify-all")}</a><select id="batch_#{id}">#{options[:mass_editable_fields].map do |field|
-                %{<option value="#{field.to_s}">#{I18n.t("papermill.#{field.to_s}", :default => field.to_s)}</option>}
-              end.join("\n")}</select>}
-      html[:dashboard][:mass_delete] = %{<a onclick="mass_delete('#{id}', '#{@template.escape_javascript I18n.t("papermill.delete-all-confirmation")}'); return false;" style="cursor:pointer">#{I18n.t("papermill.delete-all")}</a>}
-      html[:dashboard] = @template.content_tag(:ul, options[:dashboard].map{|action| @template.content_tag(:li, html[:dashboard][action], :class => action.to_s) }.join("\n"), :class => "dashboard")
+    create_url_options.merge!({ :assetable_id => assetable_id, :assetable_type => assetable_type }) if assetable_id
+    create_url_options.merge!({ :assetable_key => key }) if key
+    @template.url_for(create_url_options)
+  end
+  
+  def set_papermill_inline_js(dom_id, create_url, options)
+    return unless options[:inline_css]
+    @template.content_for :papermill_inline_js do
+      %{
+        jQuery("##{dom_id}").sortable({
+          update:function(){
+            jQuery.ajax({
+              async: true, 
+              data: jQuery(this).sortable('serialize'), 
+              dataType: 'script', 
+              type: 'post', 
+              url: '#{@template.controller.send("sort_papermill_path")}'
+            })
+          }
+        })
+      } if options[:gallery]
     end
-    
-    @template.content_for :papermill_inline_js do %{
-      jQuery("##{id}").sortable({
-        update:function(){
-          jQuery.ajax({
-            async: true, 
-            data: jQuery(this).sortable('serialize'), 
-            dataType: 'script', 
-            type: 'post', 
-            url: '#{@template.controller.send("sort_papermill_path")}'
-          })
-        }
-      })
-    } end if options[:gallery]
-    
-    @template.content_for :papermill_inline_js do %{
+    @template.content_for :papermill_inline_js do
+      %{
         new SWFUpload({
           post_params: {
-            "#{ActionController::Base.session_options[:key]}": "#{@template.cookies[ActionController::Base.session_options[:key]]}",
-            "authenticity_token": "#{@template.form_authenticity_token}"
+            "#{ ActionController::Base.session_options[:key] }": "#{ @template.cookies[ActionController::Base.session_options[:key]] }",
+            "authenticity_token": "#{ @template.form_authenticity_token }"
           },
-          upload_id: "#{id}",
-          upload_url: "#{@template.escape_javascript create_url}",
-          file_types: "#{options[:images_only] ? '*.jpg;*.jpeg;*.png;*.gif' : ''}",
-          file_queue_limit: "#{!options[:gallery] ? '1' : '0'}",
+          upload_id: "#{ dom_id }",
+          upload_url: "#{ @template.escape_javascript create_url }",
+          file_types: "#{ options[:images_only] ? '*.jpg;*.jpeg;*.png;*.gif' : '' }",
+          file_queue_limit: "#{ !options[:gallery] ? '1' : '0' }",
           file_queued_handler: Upload.file_queued,
           file_dialog_complete_handler: Upload.file_dialog_complete,
           upload_start_handler: Upload.upload_start,
@@ -152,12 +130,28 @@ module ActionView::Helpers::FormTagHelper
           upload_error_handler: Upload.upload_error,
           upload_success_handler: Upload.upload_success,
           upload_complete_handler: Upload.upload_complete,
-          button_placeholder_id: "browse_for_#{id}",
-          #{options[:swfupload].map{ |key, value| "#{key}: #{(value.is_a?(String) ? "\"#{@template.escape_javascript(value)}\"" : @template.escape_javascript(value.to_s))}" if value }.compact.join(",\n")}
+          button_placeholder_id: "browse_for_#{dom_id}",
+          #{ options[:swfupload].map { |key, value| "#{key}: #{(value.is_a?(String) ? "\"#{@template.escape_javascript(value)}\"" : @template.escape_javascript(value.to_s))}" if value }.compact.join(",\n") }
         });
       }
-  	end
-    
-	  %{<div class="papermill">#{@timestamp_field.to_s + options[:form_helper_elements].map{|element| html[element] || ""}.join("\n")}</div>}
+    end
+  end
+  
+  def set_papermill_inline_css(dom_id, width, height, options)
+    html = ["\n"]
+    size = [width && "width:#{width}px", height && "height:#{height}px"].compact.join("; ")
+    if og = options[:gallery]
+      vp, hp, vm, hm, b = [og[:vpadding], og[:hpadding], og[:vmargin], og[:hmargin], og[:border_thickness]].map &:to_i
+      gallery_width = (og[:width] || width) && "width:#{og[:width] || og[:columns]*(width+(hp+hm+b)*2)}px;" || ""
+      gallery_height = (og[:height] || height) && "min-height:#{og[:height] || og[:lines]*(height+(vp+vm+b)*2)}px;" || ""
+      html << %{##{dom_id} { #{gallery_width} #{gallery_height} }}
+      html << %{##{dom_id} .asset { margin:#{vm}px #{hm}px; border-width:#{b}px; padding:#{vp}px #{hp}px; #{size}; }}
+    else
+      html << %{##{dom_id}, ##{dom_id} .asset { #{size} }}
+    end
+    html << %{##{dom_id} .name { width:#{width || "100"}px; }}
+    @template.content_for :papermill_inline_css do
+      html.join("\n")
+    end
   end
 end
