4c487c05ed1e40d78a15e0e0799f2a1597322093
Benoit Bénézech <ben@ben.(none)>
2010-03-07 21:15:41 +0100

    activerecord stuff

--- lib/papermill/papermill.rb
+++ lib/papermill/papermill.rb
@@ -23,40 +23,35 @@ module Papermill
   module ClassMethods
     attr_reader :papermill_options
     
+    def timestamp
+      @timestamp ||= "-#{(Time.now.to_f * 1000).to_i.to_s[4..-1]}"
+    end
+    
     def papermill(*args)
       assoc_key = args.shift.to_sym
       (@papermill_options ||= {}).merge!( { assoc_key => Papermill::options.deep_merge(args.shift || {}) } )
       join_table = papermill_options[assoc_key][:through] && :papermill_associations
       
       if join_table
-        self.class_eval do 
-          has_many join_table, :as => "assetable", :dependent => :destroy, :order => :position, :conditions => { :assetable_key => assoc_key.to_s }
-          has_many assoc_key, :through => :papermill_associations, :source => :papermill_asset
-        end
-      else
-        self.class_eval do 
-          has_many assoc_key, :as => "assetable", :dependent => :destroy, :order => :position, :class_name => "PapermillAsset", :conditions => { :assetable_key => assoc_key.to_s }
-        end
-      end
-      
-      self.class_eval %{
-        after_save :set_papermill_associations_for_#{assoc_key}#, :if => proc { ! @#{assoc_key}_ids.blank? }
-        def #{assoc_key}_ids=(ids)
-          @#{assoc_key}_ids = ids.map(&:to_i).select{|i|i>0}
-          self.#{assoc_key} = PapermillAsset.find(@#{assoc_key}_ids)
-        end
-      }
-      
-      if join_table
-        self.class_eval %{
+        self.class_eval %{ 
+          has_many(join_table, :as => "assetable", :dependent => :destroy, :order => :position)
+          has_many(assoc_key, :through => :papermill_associations, :source => :papermill_asset, :conditions => "papermill_associations.assetable_key = '#{assoc_key}'") 
           
-          # TODO TEST FOR REAL
-          # SEE WHY CALLBACKS ARE FIRED MORE THAN ONCE
+          after_save :set_papermill_associations_for_#{assoc_key}
+          
+          def #{assoc_key}_ids=(ids)
+            assets_ids = ids.map(&:to_i).select{|i|i>0}
+            unless assets_ids == self.#{assoc_key}.map(&:id)
+              assets = PapermillAsset.find(assets_ids)
+              self.#{assoc_key} = assets_ids.map{|asset_id| assets.select{|asset|asset.id == asset_id}.first}
+              @#{assoc_key}_ids = assets_ids
+            end
+          end
           
           def set_papermill_associations_for_#{assoc_key}
             return if (ids = @#{assoc_key}_ids).blank?
+
             # Let's get all the associations that COULD have been associated with current assetable AND *key*
-            
             papermill_associations = PapermillAssociation.find_by_sql("\
               SELECT id, papermill_asset_id \
               FROM papermill_associations \
@@ -71,20 +66,26 @@ module Papermill
             filtered_papermill_associations = ids.map{ |papermill_asset_id| [assoc_hash.delete((papermill_association_id = assoc_hash.index(papermill_asset_id))) && papermill_association_id, papermill_asset_id] } 
             @index = 0
             PapermillAssociation.update_all("\
-              position = (CASE papermill_asset_id \#{filtered_papermill_associations.map{|assoc_id, pa_id| " WHEN \#{pa_id} THEN \#{@index += 1} " } } END), \
+              position = (CASE papermill_asset_id \#{filtered_papermill_associations.map{|assoc_id, asset_id| " WHEN \#{asset_id} THEN \#{@index += 1} " } } END), \
               assetable_key = '#{assoc_key}'",
               { :id => filtered_papermill_associations.map(&:first) })
           end
         }
       else
-        self.class_eval %{
-          def set_papermill_associations_for_#{assoc_key}
-            return if (ids = @#{assoc_key}_ids).blank?
-            @index = 0
-            PapermillAsset.update_all("\
-              position = (CASE id \#{ids.map{|i| " WHEN \#{i} THEN \#{@index += 1} " } } END), \
-              assetable_key = '#{assoc_key}'", 
-              :id => ids)
+        self.class_eval %{ 
+          has_many assoc_key, :as => "assetable", :dependent => :destroy, :order => :position, :class_name => "PapermillAsset", :conditions => {:assetable_key => assoc_key.to_s}, :before_add => Proc.new{|a, asset| asset.assetable_key = '#{assoc_key}'}
+          
+          def #{assoc_key}_ids=(ids)
+            return if (assets_ids = ids.map(&:to_i).select{|i|i>0}) == self.#{assoc_key}.map(&:id)
+            assets = PapermillAsset.find(assets_ids).each do |asset|
+              asset.position = assets_ids.index(asset.id) + 1
+            end
+            self.#{assoc_key} = assets.sort_by(&:position)
+            if !self.new_record? && self.valid?
+              moved_assets = assets.select {|a| a.position_changed? }
+              PapermillAsset.update_all("position = CASE id " + assets.map{|a| " WHEN " + a.id.to_s + " THEN " + a.position.to_s }.join + " END",
+                 :id => moved_assets.map(&:id)) unless moved_assets.empty?
+            end
           end
         }
       end
